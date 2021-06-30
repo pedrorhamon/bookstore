0fba06fb5c48718873a45a36319953532612261d
pedrorhamon <pedrorhamon16@gmail.com>
2021-06-29 21:20:56 -0300

    Corrigindo erro

---
+++ src/main/java/com/pedro/bookstore/config/DevConfig.java
@@ -0,0 +1,29 @@
+package com.pedro.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.pedro.bookstore.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instanciaBaseDados() {
+		if (strategy.equals("create")) {
+			this.dbService.instaciaBaseDados();
+		}
+		return false;
+	}
+
+}
